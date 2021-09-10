7963e7886bb43c74968ab10ec2cab19cc7771460
Hubert MUGABO <mugberto@gmail.com>
2021-09-11 01:31:56 +0200

    Add details reducer

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,10 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import detailsReducer from './details/details';
 import homeReducer from './home/home';
 
 const rootReducer = combineReducers({
   homeData: homeReducer,
+  detailsData: detailsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
---
+++ src/redux/details/details.js
@@ -0,0 +1,35 @@
+const ADD_DATA = 'firm-profile/details/ADD_DATA';
+
+const defaultState = [];
+
+const detailsReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case ADD_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const addData = (payload) => ({
+  type: ADD_DATA,
+  payload,
+});
+
+export const loadData = (sympol) => (dispatch) => {
+  fetch(`https://financialmodelingprep.com/api/v3/profile/${sympol}?apikey=78f718bb1f98542c7f205b7a95b64c04`)
+    .then((presponse) => presponse.json())
+    .then((profile) => {
+      fetch(`https://financialmodelingprep.com/api/v3/historical-chart/1hour/${sympol}?apikey=78f718bb1f98542c7f205b7a95b64c04`)
+        .then((cresponse) => cresponse.json())
+        .then((chart) => {
+          const detailsData = {
+            profileData: profile[0],
+            chartData: chart,
+          };
+          dispatch(addData(detailsData));
+        });
+    });
+};
+
+export default detailsReducer;
