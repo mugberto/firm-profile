8c44d867dee73c2f76c091ad2e90fb43aeb949c8
Hubert MUGABO <mugberto@gmail.com>
2021-09-11 00:21:10 +0200

    Configure the store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import HomePage from './components/HomePage';
-import './App.css';
 import Header from './components/Header';
 import DetailsPage from './components/DetailsPage';
+import store from './redux/configStore';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/details/:symbol">
-          <DetailsPage />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/details/:symbol">
+            <DetailsPage />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
--- src/redux/home/home.js
+++ src/redux/home/home.js
@@ -2,10 +2,10 @@ const ADD_DATA = 'firm-profile/home/ADD_DATA';
 
 const defaultState = [];
 
-const homeReducer = (state=defaultState, action) => {
-  switch(action.type) {
+const homeReducer = (state = defaultState, action) => {
+  switch (action.type) {
     case ADD_DATA:
-      return payload;
+      return action.payload;
     default:
       return state;
   }
@@ -16,8 +16,10 @@ const addData = (payload) => ({
   payload,
 });
 
-const loadData = () => (dispatch) => {
+export const loadData = () => (dispatch) => {
   fetch('https://financialmodelingprep.com/api/v3/stock/list?apikey=78f718bb1f98542c7f205b7a95b64c04')
     .then((response) => response.json())
     .then((json) => dispatch(addData(json)));
-}
+};
+
+export default homeReducer;
