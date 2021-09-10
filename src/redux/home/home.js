67db9dcd42199a2d16ea49010a1d521ca88d6772
Hubert MUGABO <mugberto@gmail.com>
2021-09-11 00:08:49 +0200

    Add homeReducer

---
+++ src/redux/home/home.js
@@ -0,0 +1,23 @@
+const ADD_DATA = 'firm-profile/home/ADD_DATA';
+
+const defaultState = [];
+
+const homeReducer = (state=defaultState, action) => {
+  switch(action.type) {
+    case ADD_DATA:
+      return payload;
+    default:
+      return state;
+  }
+};
+
+const addData = (payload) => ({
+  type: ADD_DATA,
+  payload,
+});
+
+const loadData = () => (dispatch) => {
+  fetch('https://financialmodelingprep.com/api/v3/stock/list?apikey=78f718bb1f98542c7f205b7a95b64c04')
+    .then((response) => response.json())
+    .then((json) => dispatch(addData(json)));
+}
